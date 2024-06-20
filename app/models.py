95dfba9cf4befdf8ee31291727899d4f39e61ae1
fom78Dev <fom78a@gmail.com>
2024-06-19 22:21:38 -0300

    elimino archivo models

--- app/models.py
+++
@@ -1,8 +0,0 @@
-class Movie:
-    def __init__(self, id, title, release_year, adult=False):
-        self.id = id
-        self.title = title
-        self.release_year = release_year
-        self.adult = adult
-
-        
